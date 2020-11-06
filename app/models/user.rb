acc1b2e5a6c2ee573a957bebb967d7fb078d2970
yamamokou3 <alsk.89vbm25@gmail.com>
2020-11-06 18:33:51 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+  with_options presence: true, format: { with: /\A[ァ-ン一-龥]+\z/} do
   validates :first_name, presence: true
   validates :last_name, presence: true
+  end
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/} do
   validates :first_name_kana, presence: true
   validates :last_name_kana, presence: true
+  end
   validates :birthday, presence: true
 end
