209c1e46aff90265e5bfadfd7e8f3cc4cfb06726
yamamokou3 <alsk.89vbm25@gmail.com>
2020-11-09 17:20:09 +0900

    商品を出品できるように設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else  
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :categroy_id, :status_id, :burden_id, :area_id, :days_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,22 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :categroy
+  belongs_to_active_hash :status
+  belongs_to_active_hash :burden
+  belongs_to_active_hash :area
+  belongs_to_active_hash :day
+  belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :categroy_id, numericality: { other_than: 1 }
+    validates :status_id, numericality: { other_than: 1 }
+    validates :burden_id, numericality: { other_than: 1 }
+    validates :area_id, numericality: { other_than: 1 }
+    validates :days_id, numericality: { other_than: 1 }
+    validates :price
+  end
 end
