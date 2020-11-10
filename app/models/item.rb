90d465c49100096aa6d09a80500e60131ff10ae5
yamamokou3 <alsk.89vbm25@gmail.com>
2020-11-10 17:39:36 +0900

    販売手数料、販売利益の計算を実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,13 +10,14 @@ class Item < ApplicationRecord
 
   with_options presence: true do
     validates :image
-    validates :name
-    validates :description
+    validates :name, length: { maximum: 40}
+    validates :description, length: { maximum: 1000}
     validates :categroy_id, numericality: { other_than: 1 }
     validates :status_id, numericality: { other_than: 1 }
     validates :burden_id, numericality: { other_than: 1 }
     validates :area_id, numericality: { other_than: 1 }
     validates :days_id, numericality: { other_than: 1 }
-    validates :price
+    validates :price, format: { with: /\A[0-9]+\z/ }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   end
 end
